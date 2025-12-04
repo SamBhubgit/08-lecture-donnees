fb7d4ae2d19ccf685f9ea6ed0d2857c7f312edb9
Samuel-BOUCHER <samuel.boucher@edu.esiee.fr>
2025-12-04 09:37:02 +0000

    LectureDonnees

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #### Imports et définition des variables globales
-
+"""
+Ce programme permet de lire des fichiers csv
+"""
+import csv
 FILENAME = "listes.csv"
 
 #### Fonctions secondaires
@@ -13,34 +16,57 @@ def read_data(filename):
     Returns:
         list: le contenu du fichier (1 list par ligne)
     """
-    l = []
-    return l
+    with open(filename,mode="r",encoding="utf8") as f:
+        lignes=list(csv.reader(f,delimiter=";"))
+        for i in range(len(lignes)):
+            for j in range(len(lignes[i])):
+                lignes[i][j]=int(lignes[i][j])
+    return lignes
 
 def get_list_k(data, k):
-    l = []
-    return l
+    """
+    Cette fonction permet de retourner une ligne de la liste principale
+    """
+    return data[k]
 
 def get_first(l):
-    return None
+    """
+    Cette fonction permet de retourner le premier élément d'une liste
+    """
+    return l[0]
 
 def get_last(l):
-    return None
+    """
+    Cette fonction permet de retourner le dernier élément d'une liste
+    """
+    return l[-1]
 
 def get_max(l):
-    return None
+    """
+    Cette fonction permet de retourner l'élément maximum d'une liste
+    """
+    return max(l)
 
 def get_min(l):
-    return None
+    """
+    Cette fonction permet de retourner l'élément minimum d'une liste
+    """
+    return min(l)
 
 def get_sum(l):
-    return None
+    """
+    Cette fonction permet de retourner la somme des éléments d'une liste
+    """
+    return sum(l)
 
 
 #### Fonction principale
 
 
 def main():
-    pass
+    """
+    Cette fonction permet de faire des tests
+    """
     # data = read_data(FILENAME)
     # for i, l in enumerate(data):
     #     print(i, l)
